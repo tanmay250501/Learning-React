79f246dcd5f1b3b55c74cf43ad5064c86557782c
Tanmay K. Sawwalakhe <tkdsawwalakhe01@gmail.com>
2024-06-12 14:45:47 +0530

    This commit will change some data in App.js folder

--- App.js
+++ App.js
@@ -1,5 +1,29 @@
-const heading = React.createElement("h1" , {id : "heading"} , "Hii from React");
+/**
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1>I am h1 tag</h1>
+ *          <h2>I am h1 tag</h2>
+ *      </div>
+ * </div>
+ * 
+ * 
+ *
+ * 
+ * */
+
+const parent = React.createElement("div", { id: "parent" }, 
+    React.createElement("div", { id: "child" }, 
+        [   React.createElement("h1", {}, "I am an h1 tag"), 
+            React.createElement("h2", {}, "I am an h2 tag")
+        ]
+    )
+)
+
+
+
+
+// const heading = React.createElement("h1", { id: "heading" }, "Hii from React");
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(heading)
+root.render(parent)
