355f9f9906fd16d7f5933b54db9b2f269400bda2
Tanmay K. Sawwalakhe <tkdsawwalakhe01@gmail.com>
2024-06-15 20:28:12 +0530

    started JSX

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 
 
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, 
     React.createElement("div", { id: "child" }, 
-        [   React.createElement("h1", {}, "I am an h1 tag"), 
+        [   React.createElement("h1", {}, "I am  Tanmay Sawwalakhe"), 
             React.createElement("h2", {}, "I am an h2 tag")
         ]
     )
 )
 
+//JSX (transpiled before it6 reaches JS engine) - PARCEL - BABEL => its job is to convert JSX code to React Code
 
-
+//JSX => React.createElement => ReactElement - JS object => HTMLElement(render)
+const JsxHeading = <h1>Heading from JSX</h1>
 
 // const heading = React.createElement("h1", { id: "heading" }, "Hii from React");
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(parent)
+root.render(JsxHeading)
