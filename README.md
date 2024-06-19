79f7b2ef0faec2142656b3a1c1362838a1f33c17
Tanmay K. Sawwalakhe <tkdsawwalakhe01@gmail.com>
2024-06-19 15:01:17 +0530

    changed the path for App.js file and made code clean

--- README.md
+++ README.md
@@ -73,3 +73,48 @@ It is configuration of npm & It keeps track of what exact version of that packag
 
 # Package-lock.json
 It keeps track of exact version of the dependencies
+
+## Notes :
+
+```jsx
+const parent = React.createElement(
+  "div", 
+  { id: "parent" },
+  React.createElement(
+    "div", 
+    { id: "child" },
+    [
+      React.createElement("h1", {}, "I am Tanmay Sawwalakhe"),
+      React.createElement("h2", {}, "I am an h2 tag")
+    ]
+  )
+);
+
+use thsi brackets () to specify that jsx is writen under this bracket
+
+const heading = React.createElement("h1", { id: "heading" }, "Hii from React");
+
+
+
+react Functional Component 
+React component => means a function which returns reactCode or some piece of jsx
+
+const ReactHeading = () => (
+    <h1>Namste react heading from functional component 😍</h1>
+)
+
+Component Composition => It is a proccess of combinning smaller , resuable compoents together to create a larger or more complex component
+
+const Divisionheading = () => (
+    <div className="heading">
+        {1000 + 2000}
+        <h1>Hi from Division Heading</h1>
+        {ReactHeading()}
+        <ReactHeading />
+    </div>
+)
+```
+
+JSX (transpiled before it6 reaches JS engine) - PARCEL - BABEL => its job is to convert JSX code to React Code
+
+JSX => React.createElement => ReactElement - JS object => HTMLElement(render)
--- src/App.js
+++ src/App.js
@@ -3,41 +3,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const parent = React.createElement("div", { id: "parent" },
-//     React.createElement("div", { id: "child" },
-//         [React.createElement("h1", {}, "I am  Tanmay Sawwalakhe"),
-//         React.createElement("h2", {}, "I am an h2 tag")
-//         ]
-//     )
-// )
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },
+        [React.createElement("h1", {}, "I am  Tanmay Sawwalakhe"),
+        React.createElement("h2", {}, "I am an h2 tag")
+        ]
+    )
+)
 
-//JSX (transpiled before it6 reaches JS engine) - PARCEL - BABEL => its job is to convert JSX code to React Code
 
-//JSX => React.createElement => ReactElement - JS object => HTMLElement(render)
 const JsxHeading = (<h1>Heading from JSX</h1>)
 
-// use thsi brackets () to specify that jsx is writen under this bracket
 
-// const heading = React.createElement("h1", { id: "heading" }, "Hii from React");
-
-
-
-// react Functional Component 
-// React component => means a function which returns reactCode or some piece of jsx
-
-// const ReactHeading = () => (
-//     <h1>Namste react heading from functional component 😍</h1>
-// )
-
-// Component Composition => It is a proccess of combinning smaller , resuable compoents together to create a larger or more complex component
-// const Divisionheading = () => (
-//     <div className="heading">
-//         {1000 + 2000}
-//         <h1>Hi from Division Heading</h1>
-//         {ReactHeading()}
-//         <ReactHeading />
-//     </div>
-// )
 
 const Header = () => {
     return (
